74f369697a9a6cce055540f8df48669399b2c5e9
joemen <y7cheng@eng.ucsd.edu>
2019-06-02 18:25:46 -0700

    Add settings

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,34 +1,25 @@
-from django.db import models
-from django.forms import ModelForm
-
-# read the django docs on models: 
+import pickle
+from sklearn.feature_extraction.text import CountVectorizer
+# read the django docs on models:
 # https://docs.djangoproject.com/en/dev/topics/db/models/
 
 # a model for an airport
-class Airport(models.Model):
-    '''class representing an airport'''
 
-    # airport letter code (usu 3 or 4 letters)
-    code = models.CharField('Code', max_length=10)
-    # long name (city plus airport name)
-    longname = models.CharField('Name', max_length=100)
-    # airport latitude in degrees
-    latitude = models.DecimalField('Latitude', max_digits=8, decimal_places=5)
-    # airport longitude in degrees
-    longitude = models.DecimalField('Latitude', max_digits=8, decimal_places=5)
-
-    # methods
+class Airport():
     def get_id(self):
         return self.id
 
-    def get_code(self):
-        return self.code + "!"
+    def get_code(self, input):
+        filename = 'finalized_model.sav'
+        data = []
+        data.append(input)
 
-    def get_name(self):
-        return self.longname
+        count_Vector = CountVectorizer()
+        X = count_Vector.fit_transform(data)
 
-    def get_lat(self):
-        return self.latitude
 
-    def get_lon(self):
-        return self.longitude
+
+        loaded_model = pickle.load(open(filename, 'rb'))
+        yp = loaded_model.predict(X)
+
+        return yp
